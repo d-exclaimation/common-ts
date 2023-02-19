a307f94c480c2757d3ff007986397a434713849e
Vincent <thisoneis4business@gmail.com>
2023-02-19 13:47:13 +1300

    feat(v8): Termination for AsyncIterators

--- v8/iterator.ts
+++ v8/iterator.ts
@@ -14,10 +14,13 @@ export class AsyncChannel<T> implements AsyncIterator<T> {
   private pushQueue: T[];
   private running: boolean;
 
+  private terminations: ((reason: "return" | "error" | "finish") => void)[];
+
   constructor() {
     this.pullQueue = [];
     this.pushQueue = [];
     this.running = true;
+    this.terminations = [];
   }
 
   async next(): Promise<IteratorResult<T>> {
@@ -25,10 +28,12 @@ export class AsyncChannel<T> implements AsyncIterator<T> {
   }
 
   async return(): Promise<IteratorResult<T>> {
+    this.close("return");
     return { value: undefined, done: true };
   }
 
   async throw?(e?: any): Promise<IteratorResult<T>> {
+    this.close("error");
     return Promise.reject(e);
   }
 
@@ -46,9 +51,23 @@ export class AsyncChannel<T> implements AsyncIterator<T> {
    * @returns void
    */
   public finish(): void {
-    this.empty();
+    this.close("finish");
   }
 
+  /**
+   * Register a termination callback
+   * @param terminate The termination callback
+   */
+  public onTerminate(
+    terminate: (reason: "return" | "error" | "finish") => void
+  ): void {
+    this.terminations.push(terminate);
+  }
+
+  /**
+   * Push a value to the channel
+   * @param value The value to push
+   */
   private push(value: T): void {
     const pulling = this.pullQueue.shift();
     if (pulling) {
@@ -59,6 +78,10 @@ export class AsyncChannel<T> implements AsyncIterator<T> {
     this.pushQueue.push(value);
   }
 
+  /**
+   * Pull the next value or wait for the next value
+   * @returns A promise of the next value
+   */
   private pull(): Promise<IteratorResult<T>> {
     return new Promise<IteratorResult<T>>((resolve) => {
       const value = this.pushQueue.shift();
@@ -73,7 +96,11 @@ export class AsyncChannel<T> implements AsyncIterator<T> {
     });
   }
 
-  private empty() {
+  /**
+   * Terminates the channel with a reason
+   * @param reason The reason of termination
+   */
+  private close(reason: "return" | "error" | "finish") {
     if (!this.running) {
       return;
     }
@@ -83,6 +110,7 @@ export class AsyncChannel<T> implements AsyncIterator<T> {
     );
     this.pullQueue.length = 0;
     this.pushQueue.length = 0;
+    this.terminations.forEach((terminate) => terminate(reason));
   }
 
   /**
@@ -118,7 +146,27 @@ export class AsyncStream<T> implements AsyncIterator<T> {
     return Promise.reject(e);
   }
 
+  /**
+   * Async Iterator
+   * @returns Async Iterator
+   */
   public [Symbol.asyncIterator](): AsyncIterator<T> {
     return this.channel;
   }
 }
+
+/**
+ * Create a new Async Channel
+ * @template T The type of the channel
+ * @returns Async Channel
+ */
+export const asyncChannel = <T>() => new AsyncChannel<T>();
+
+/**
+ * Create a new Async Stream
+ * @template T The type of the channel
+ * @param exec The execution function that would be called with the channel
+ * @returns Async Stream
+ */
+export const asyncStream = <T>(exec: (cont: AsyncChannel<T>) => void) =>
+  new AsyncStream<T>(exec);
