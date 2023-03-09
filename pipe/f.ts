8e2336a0caef2d64a861ef835e482812e721bd63
Vincent <thisoneis4business@gmail.com>
2023-03-10 10:40:02 +1300

    feat(pipe): Uncurry function

--- pipe/f.ts
+++ pipe/f.ts
@@ -22,3 +22,24 @@ export function f<T extends FuncLike>(
 ): PipeableFunc<HeadIn<T>, ReturnType<T>> {
   return (value) => func(value, ...args);
 }
+
+type Uncurry<T, Acc extends any[] = []> = T extends (arg: infer Arg) => infer R
+  ? Uncurry<R, [...Acc, Arg]>
+  : (...args: Acc) => T;
+
+/**
+ * Create a function that uncurry the given function arguments
+ * @param func Function to be uncurried
+ * @returns A function that take all the arguments at once
+ */
+export function b<Arg, T>(func: (arg: Arg) => T): Uncurry<(arg: Arg) => T>;
+export function b(func: (arg: any) => any): (...args: any[]) => any {
+  return (...args) => {
+    const [arg, ...rest] = args;
+    if (rest.length === 0) return func(arg);
+
+    // Don't mind this, it's just to make the type checker happy :)
+    const rec = b(func(arg)) as (...args: any[]) => any;
+    return rec(...rest);
+  };
+}
