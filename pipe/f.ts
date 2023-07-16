73fbcb65a9f79667d5f59185a0d4bf27df964aae
Vincent <thisoneis4business@gmail.com>
2023-07-16 22:12:17 +1200

    feat(pipe): fn and fnx (dual typing function)

--- pipe/f.ts
+++ pipe/f.ts
@@ -43,3 +43,75 @@ export function b(func: (arg: any) => any): (...args: any[]) => any {
     return rec(...rest);
   };
 }
+
+/**
+ * Create a function that allow the head argument to be provided later (as a curried function) or normally
+ *
+ * **Limitation**: Generic type cannot be inferred, use `fnx` instead
+ *
+ * @param arity The number of arguments to be injected
+ * @param fn The function to be curried
+ * @returns A curried function that take the head argument and also the original function itself
+ * @example
+ * ```ts
+ * const join = fn(2, (strings: string[], separator: string) => strings.join(separator));
+ *
+ * join(["Hello", "World"], " "); // "Hello World" (normal)
+ * join(" ")(["Hello", "World"]); // "Hello World" (curried)
+ *
+ * // (pipeable)
+ * pipe(
+ *  ["Hello", "World"],
+ *  join(" "),
+ * );
+ * ```
+ */
+export function fn<Fn extends (...args: any[]) => any>(arity: number, fn: Fn) {
+  return fnx<
+    (...tail: TailIns<Fn>) => (head: HeadIn<Fn>) => ReturnType<Fn>,
+    Fn
+  >(arity, fn);
+}
+
+/**
+ * Create a function that allow the head argument to be provided later (as a curried function) or normally (generic version)
+ * @param arity The number of arguments to be injected
+ * @param body The function to be curried
+ * @returns A curried function that take the head argument and also the original function itself
+ * @example
+ * ```ts
+ * const tap = fnx<
+ *  <T>(fn: (arg: T) => void) => (arg: T) => T,
+ *  <T>(fn: (arg: T) => void, arg: T) => T
+ * >(2, (fn, arg) => {
+ *  fn(arg);
+ *  return arg;
+ * });
+ *
+ * tap("Hello", console.log); // "Hello" (normal)
+ * tap(console.log)("Hello"); // "Hello" (curried)
+ *
+ * // (pipeable)
+ * pipe(
+ *  "Hello",
+ *  tap(console.log),
+ * );
+ * ```
+ */
+export function fnx<
+  DataLast extends (...args: Array<any>) => any,
+  DataFirst extends (...args: Array<any>) => any
+>(
+  arity: Parameters<DataFirst>["length"],
+  body: DataFirst
+): DataLast & DataFirst {
+  const isDataFirst: (args: IArguments) => boolean = (args) =>
+    args.length >= arity;
+  return function () {
+    if (isDataFirst(arguments)) {
+      // @ts-expect-error
+      return body.apply(this, arguments);
+    }
+    return ((self: any) => body(self, ...arguments)) as any;
+  } as any;
+}
